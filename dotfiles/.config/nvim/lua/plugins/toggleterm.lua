f4f15f1f64bc04906a8353d8a2ed6e80d6a85c2c
Hoang Quoc Trung <hoangquoctrung02@hotmail.com>
2024-11-05 11:26:20 +0700

    change toggleterm init dir to current dir

--- dotfiles/.config/nvim/lua/plugins/toggleterm.lua
+++ dotfiles/.config/nvim/lua/plugins/toggleterm.lua
@@ -14,13 +14,18 @@ return {
       { "<leader>0", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
       {
         "<leader>td",
-        "<cmd>ToggleTerm size=30 dir=~/Desktop direction=horizontal<cr>",
-        desc = "Open a smaller horizontal terminal at the Desktop directory",
+        "<cmd>ToggleTerm size=30 direction=horizontal<cr>",
+        desc = "Open a horizontal terminal",
       },
       {
         "<leader>tv",
         "<cmd>ToggleTerm size=30 direction=vertical<cr>",
         desc = "Open a vertical terminal",
+      },
+      {
+        "<leader>tf",
+        "<cmd>ToggleTerm size=30 direction=float<cr>",
+        desc = "Open a float terminal",
       },
     },
   },
